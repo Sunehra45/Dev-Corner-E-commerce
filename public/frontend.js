4c086acb86e1df4c42a3bb660f6096bbd622563b
Sunehra Tabassum <sunehrat815@gmail.com>
2025-03-20 21:35:16 +0500

    Filter feature is Built

--- public/frontend.js
+++ public/frontend.js
@@ -8,7 +8,6 @@ BuyBtn.forEach(btn => {
     })
 });
 
-
 //navbar bg active
 window.addEventListener('scroll',function(){
 if(this.window.scrollY > window.innerHeight){
@@ -19,22 +18,57 @@ else {
 }
 })
 
-//toggle filter section:
+// filter section:
 let openfilter = document.querySelector('.open-filter'); 
 let closefilter = document.querySelector('.close-btn');
 
 function toggleFilter() {
  document.getElementById("filterBox").classList.toggle("hidden");
-
+ if(toggleFilter){
+    if (openfilter.innerText === "Show Filter") {
+        openfilter.innerText = "Hide Filter";
+    } else {
+        openfilter.innerText = "Show Filter";
+    }
+}
 }
 closefilter.addEventListener('click',toggleFilter);
 openfilter.addEventListener('click',()=>{
     toggleFilter();
-    if(toggleFilter){
-        if (openfilter.innerText === "Show Filter") {
-            openfilter.innerText = "Hide Filter";
-        } else {
-            openfilter.innerText = "Show Filter";
-        }
-    }
+   
 });
+
+let checkboxes = document.querySelectorAll("input[type='checkbox']");
+let filterOptions = document.querySelector('.slection');
+
+checkboxes.forEach(checkbox => {
+    checkbox.addEventListener("change",()=>{
+
+    let newOption =  document.createElement('div');
+    newOption.classList.add('selected');
+    newOption.innerText = checkbox.value  + " ";
+    newOption.style.visibility = "visible"
+
+    let removeBtn = document.createElement("span");
+    removeBtn.innerText = "X";
+    removeBtn.style.fontWeight = "800";
+    removeBtn.style.cursor = "pointer";
+
+    removeBtn.addEventListener("click", () => {
+        newOption.remove();
+        checkbox.checked = false; // Uncheck the checkbox if needed
+    });  
+
+    newOption.appendChild(removeBtn);
+
+    if (filterOptions) {
+        filterOptions.appendChild(newOption);
+    }
+    })
+});
+
+
+let removeFilter = document.querySelectorAll('#cross');
+// removeFilter.addEventListener('click',()=>{
+
+// })
