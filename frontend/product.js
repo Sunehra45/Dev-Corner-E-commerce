c31a625f1f19a6e6731d9b3467ffd4860db6ddf9
Sunehra Tabassum <sunehrat815@gmail.com>
2025-04-29 17:36:50 +0500

    ProductPage  CSS is fixed

--- frontend/product.js
+++ frontend/product.js
@@ -2,13 +2,13 @@ let info = JSON.parse(localStorage.getItem("product"));
 console.log(info)
 
 let productName = document.querySelector('h1');
-let productDescription = document.querySelector('h3');
-// let productName = document.querySelector('h1');
+let productDescription = document.querySelector('p');
 let productPrice = document.querySelector('h2');
-// let productName = document.querySelector('h1');
+let productcategory = document.querySelector('h4');
+let productImage = document.querySelector('img');
+
 productName.innerText = info.name;
 productDescription.innerText = info.description;
 productPrice.innerText = `${info.price} PKR`;
-
-
-
+productImage.src = info.image.replace('/uploads/', '/images/');
+productcategory.innerText = info.category;
