713071917099f4c7c611276f4a369209676f92a4
Sunehra Tabassum <sunehrat815@gmail.com>
2025-04-30 12:15:27 +0500

    Zoom effect JS Added

--- frontend/product.js
+++ frontend/product.js
@@ -1,5 +1,4 @@
 let info = JSON.parse(localStorage.getItem("product"));
-console.log(info)
 
 let productName = document.querySelector('h1');
 let productDescription = document.querySelector('p');
@@ -12,3 +11,23 @@ productDescription.innerText = info.description;
 productPrice.innerText = `${info.price} PKR`;
 productImage.src = info.image.replace('/uploads/', '/images/');
 productcategory.innerText = info.category;
+
+
+let imageZoom = document.querySelector('.leftcontent');
+imageZoom.addEventListener('mousemove', (e) => {
+
+    imageZoom.style.setProperty('--display', 'block');
+    imageZoom.style.setProperty('--imageurl', `url("${productImage.src}")`);
+
+    let pointer = {
+       x: (e.offsetX * 100) / imageZoom.offsetWidth,
+       y: (e.offsetY * 100) / imageZoom.offsetHeight
+    }
+
+    imageZoom.style.setProperty('--zoom-x', pointer.x + '%');
+    imageZoom.style.setProperty('--zoom-y', pointer.y + '%');
+
+    imageZoom.addEventListener('mouseout', () => {
+        imageZoom.style.setProperty('--display', 'none');
+    })
+})
